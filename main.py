7d73f82197a349befcd6873038e656ce56717f87
QuasarSerg <quasar.se@gmail.com>
2023-10-27 11:14:22 +0300

    test g4f

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-# import g4f
+import g4f
 
 from dotenv import load_dotenv
 import os
@@ -10,14 +10,15 @@ load_dotenv()
 
 @app.get("/")
 async def root():
-    # response = g4f.ChatCompletion.create(
-    #     model=g4f.models.gpt_4,
-    #     messages=[{"role": "user", "content": "What is the holiday in Russia today?"}],
-    # )
-    response = os.getenv('TELEGRAM_BOT_API_TOKEN')
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.default,
+        messages=[{"role": "user", "content": "What is the holiday in Russia today?"}],
+    )
+    # response = os.getenv('TELEGRAM_BOT_API_TOKEN')
 
     return {"message": response}
 
 
 if __name__ == '__main__':
+    print(os.getenv('TELEGRAM_BOT_API_TOKEN'))
     pass
