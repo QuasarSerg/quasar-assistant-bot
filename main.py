a239b6aa135788c1bb2a225cdf2ac5dea663f392
QuasarSerg <quasar.se@gmail.com>
2023-10-27 13:05:00 +0300

    add telebot

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-from fastapi import FastAPI
-import g4f
-
 from dotenv import load_dotenv
 import os
+import telebot
+import g4f
+from fastapi import FastAPI
 
 app = FastAPI()
 load_dotenv()
 
+bot = telebot.TeleBot(os.getenv('TELEGRAM_BOT_API_TOKEN'))
+
+bot.set_my_commands([
+    telebot.types.BotCommand("/info", 'Справка')
+])
+
 
 @app.get("/")
 async def root():
-    response = g4f.ChatCompletion.create(
-        model=g4f.models.default,
-        messages=[{"role": "user", "content": "What is the holiday in Russia today?"}],
-    )
-    # response = os.getenv('TELEGRAM_BOT_API_TOKEN')
+    return {"message": "👋"}
 
-    return {"message": response}
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == '/info':
+        bot.send_message(message.from_user.id, '👋')
+    else:
+        response = g4f.ChatCompletion.create(
+            model=g4f.models.default,
+            messages=[{"role": "user", "content": message.text}],
+        )
+        bot.send_message(message.from_user.id, response)
 
 
 if __name__ == '__main__':
-    print(os.getenv('TELEGRAM_BOT_API_TOKEN'))
-    pass
+    bot.infinity_polling()
