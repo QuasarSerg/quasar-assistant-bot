42c5458001c2c16fac24c2c9d0bdabe13382f35c
QuasarSerg <quasar.se@gmail.com>
2023-11-02 23:43:05 +0300

    web-service in a separate thread

--- main.py
+++ main.py
@@ -2,71 +2,59 @@ import asyncio
 import logging
 import sys
 import g4f
-from os import getenv
 
-from aiogram import Bot, Dispatcher, Router, types
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.utils.markdown import hbold
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from os import getenv
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, BotCommand, BotCommandScopeDefault
+from aiogram.utils.markdown import hbold
+from controller import run_web_service
 
-app = FastAPI()
 load_dotenv()
-# Bot token can be obtained via https://t.me/BotFather
-TOKEN = getenv('TELEGRAM_BOT_API_TOKEN')
+TOKEN = getenv("TELEGRAM_BOT_API_TOKEN")
 
-# All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
-
-
-@app.get("/")
-async def root():
-    return {"message": "👋"}
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
 
+@dp.message(Command("info"))
+async def command_start_handler(message: Message) -> None:
+    msg_info = ("Этот дружелюбный бот готов поддержать разговор и ответить на вопросы. "
+                "Для генерации ответов используются модели GPT.")
+    await message.answer(msg_info)
+
+
 @dp.message()
-async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
+async def echo_handler(message: Message) -> None:
     try:
-        # Send a copy of the received message
-        # await message.send_copy(chat_id=message.chat.id)
-        response = g4f.ChatCompletion.create(
+        response = await g4f.ChatCompletion.create_async(
             model=g4f.models.default,
-            messages=[{"role": "user", "content": message.text}],
+            messages=[{"role": "user", "content": message.text}]
         )
-
         await message.answer(response)
     except TypeError:
-        # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
 
 
+def get_commands():
+    return [
+        BotCommand(command='info', description='Справка')
+    ]
+
+
 async def main() -> None:
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    # And the run events dispatching
+    await bot.set_my_commands(get_commands(), BotCommandScopeDefault())
     await dp.start_polling(bot)
 
 
+run_web_service()
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
